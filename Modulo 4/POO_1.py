579f28d9a4717ec969a7f508c256d09c1033be15
nadia0207 <nadia0207@hotmail.com>
2025-03-18 23:58:06 +0100

    practicando modulo 4 POO Parte 2

--- Modulo 4/POO_1.py
+++ Modulo 4/POO_1.py
@@ -1,41 +1,60 @@
-# Definimos unos cuantos clientes
-clientes= [
-{'Nombre': 'Hector',
-'Apellidos':'Costa Guzman',
-'dni':'11111111A'},
-{'Nombre': 'Juan',
-'Apellidos':'González Márquez',
-'dni':'22222222B'}
-]
+### No intentes entender este código, sólo fíjate en cómo se utiliza abajo
+# Creo una estructura para los clientes
+class Cliente:
+    def __init__(self, dni, nombre,apellidos):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellidos = apellidos
 
-#Creamos una función que muestra un cliente en una lista a partir del DNI
-def mostrar_cliente(clientes, dni):
-    for c in clientes:
-        if (dni == c['dni']):
-            print('{} {}'.format(c['Nombre'],c['Apellidos']))
-            return
-    print('Cliente no encontrado')
+    def __str__(self):
+        return '{} {}'.format(self.nombre,self.apellidos)
     
+# Y otra para las empresas
+class Empresa:
+    def __init__(self, clientes=[]):
+        self.clientes = clientes
+        def mostrar_cliente(self, dni=None):
+            for c in self.clientes:
+                if c.dni == dni:
+                    print(c)
+                    return
+        print("Cliente no encontrado")
 
-# Creamos una función que borra un cliente en una lista a partir del DNI
-def borrar_cliente(clientes, dni):
-    for i,c in enumerate(clientes): # Recorremos la lista con un bucle for, obteniendo tanto el índice (i) como el cliente (c).
-        if (dni == c['dni']):
-            del( clientes[i] )
+def borrar_cliente(self, dni=None):
+    for i,c in enumerate(self.clientes):
+        if c.dni == dni:
+            del(self.clientes[i])
             print(str(c),"> BORRADO")
             return
-    print('Cliente no encontrado') 
-    
-   
+    print("Cliente no encontrado")
 
-### Fíjate muy bien cómo se utiliza el código estructurado
+### Ahora utilizaré ambas estructuras
+# Creo un par de clientes
+hector = Cliente(nombre="Hector",
+apellidos="Costa Guzman",
+dni="11111111A")
+juan = Cliente("22222222B", "Juan",
+"Gonzalez Marquez")
+
+# Creo una empresa con los clientesiniciales
+empresa = Empresa(clientes=[hector,
+juan])
+# Muestro todos los clientes
 print("==LISTADO DE CLIENTES==")
-print(clientes)
+print(empresa.clientes)
 print("\n==MOSTRAR CLIENTES POR DNI==")
-mostrar_cliente(clientes, '11111111A')
-mostrar_cliente(clientes, '11111111Z')
+# Consulto clientes por DNI
+
+#empresa.mostrar_cliente("11111111A")
+#empresa.mostrar_cliente("11111111Z")
+
+
 print("\n==BORRAR CLIENTES POR DNI==")
-borrar_cliente(clientes, '22222222V')
-borrar_cliente(clientes, '22222222B')
+# Borro un cliente por DNI
+
+#empresa.borrar_cliente("22222222V")
+#empresa.borrar_cliente("22222222B")
+
+# Muestro de nuevo todos los clientes
 print("\n==LISTADO DE CLIENTES==")
-print(clientes)
+print(empresa.clientes)
