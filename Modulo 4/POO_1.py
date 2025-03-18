5d5529e3d86ce640ed67d1a59cde558af3bec41a
nadia0207 <nadia0207@hotmail.com>
2025-03-19 00:16:59 +0100

    Practicando POO modulo 4 parte 3

--- Modulo 4/POO_1.py
+++ Modulo 4/POO_1.py
@@ -10,23 +10,26 @@ class Cliente:
         return '{} {}'.format(self.nombre,self.apellidos)
     
 # Y otra para las empresas
+#Enumerate se usa cuando quieres recorrer una lista y, al mismo tiempo,
+#obtener tanto el índice como el valor de cada elemento.
 class Empresa:
     def __init__(self, clientes=[]):
         self.clientes = clientes
-        def mostrar_cliente(self, dni=None):
-            for c in self.clientes:
-                if c.dni == dni:
-                    print(c)
-                    return
+        
+    def mostrar_cliente(self, dni=None):
+        for c in self.clientes:
+            if c.dni == dni:
+                print(c)
+                return
         print("Cliente no encontrado")
 
-def borrar_cliente(self, dni=None):
-    for i,c in enumerate(self.clientes):
-        if c.dni == dni:
-            del(self.clientes[i])
-            print(str(c),"> BORRADO")
-            return
-    print("Cliente no encontrado")
+    def borrar_cliente(self, dni=None):
+        for i,c in enumerate(self.clientes):
+            if c.dni == dni:
+                del(self.clientes[i])
+                print(str(c),"> BORRADO")
+                return
+        print("Cliente no encontrado")
 
 ### Ahora utilizaré ambas estructuras
 # Creo un par de clientes
@@ -36,24 +39,22 @@ dni="11111111A")
 juan = Cliente("22222222B", "Juan",
 "Gonzalez Marquez")
 
-# Creo una empresa con los clientesiniciales
-empresa = Empresa(clientes=[hector,
-juan])
+# Creo una empresa con los clientes iniciales
+empresa = Empresa(clientes=[hector,juan])
 # Muestro todos los clientes
 print("==LISTADO DE CLIENTES==")
 print(empresa.clientes)
+
 print("\n==MOSTRAR CLIENTES POR DNI==")
 # Consulto clientes por DNI
-
-#empresa.mostrar_cliente("11111111A")
-#empresa.mostrar_cliente("11111111Z")
+empresa.mostrar_cliente("11111111A")
+empresa.mostrar_cliente("11111111Z")
 
 
 print("\n==BORRAR CLIENTES POR DNI==")
 # Borro un cliente por DNI
-
-#empresa.borrar_cliente("22222222V")
-#empresa.borrar_cliente("22222222B")
+empresa.borrar_cliente("22222222V")
+empresa.borrar_cliente("22222222B")
 
 # Muestro de nuevo todos los clientes
 print("\n==LISTADO DE CLIENTES==")
